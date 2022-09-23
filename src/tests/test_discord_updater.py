29aab47d8fd88987c0450a148cd0ecbc230cfd48
Christopher A <chris.auglend@gmail.com>
2022-09-23 19:22:19 +0200

    Writing asserting that get_current_version returns the current version correctly.

--- src/tests/test_discord_updater.py
+++ src/tests/test_discord_updater.py
@@ -5,6 +5,7 @@ change. Hurray.
 DISCORD_V_PATH: Location where discord version is kept
 """
 import pytest
+import json
 from discord_updater import get_current_version
 
 
@@ -18,7 +19,22 @@ class Test_get_current_version:
         assert version == 0
 
     def test_missing_version_raises_ValueError(self, tmp_path):
+        """
+        Assert that if version is missing from file a
+        ValueError is raised.
+        """
         d = tmp_path / "test_me"
         d.touch()
         with pytest.raises(ValueError):
             get_current_version(d)
+
+    def test_returns_version(self, tmp_path):
+        """
+        Test if get_current_version returns a float
+        with the current version.
+        """
+        f = tmp_path / "build_info.json"
+        f_version = 0.20
+        version = f"0.{f_version:0.2f}"
+        f.write_text(json.dumps({"version": version}))
+        assert f_version == get_current_version(f)
