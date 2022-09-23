7a1dcd681b25219c98fed8d09d5d23aa0f2b42aa
Christopher A <chris.auglend@gmail.com>
2022-09-23 22:31:55 +0200

    Renaming filter_foo to is_ver_char.

--- src/discord_updater.py
+++ src/discord_updater.py
@@ -1,7 +1,10 @@
 import json
 
 
-def filter_foo(char):
+def is_ver_char(char):
+    """
+    Return True if character is . or numeric, False otherwise.
+    """
     return char.isnumeric() or char == "."
 
 
@@ -12,7 +15,7 @@ def parse_version(string):
     """
     if not isinstance(string, str):
         raise TypeError
-    string = "".join(filter(filter_foo, string))
+    string = "".join(filter(is_ver_char, string))
     if string == "":
         raise ValueError("String must contain version data")
     string = string.rsplit(".")
